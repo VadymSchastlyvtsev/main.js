120dae4d8905e55eb202d616fb0e6f37430c5648
VadymSchastlyvtsev <drdre6870@gmail.com>
2023-05-21 23:21:11 +0300

    s

--- lesson2.js
+++ lesson2.js
@@ -42,3 +42,78 @@
 // const firstElement = courseTopic[0];
 // const lastElement = courseTopic[20];
 // console.log(lastElement)
+
+// const products = [
+//     { name: "Radar", price: 1300, quantity: 4 },
+//     { name: "Scanner", price: 2700, quantity: 3 },
+//     { name: "Droid", price: 400, quantity: 7 },
+//     { name: "Grip", price: 1200, quantity: 9 },
+//   ];
+
+//   const ownPropName = [];
+  
+//   function getAllPropValues(propName) {
+//     // Change code below this line
+//   for (const product of products) {
+//     // console.log(product);
+//     if (product.hasOwnProperty(propName)){
+//         ownPropName.push(product[propName])
+// console.log(ownPropName);
+//     }
+//   }
+//   return ownPropName;
+  
+//     // Change code above this line
+//   }
+  
+//   getAllPropValues("name");
+//   getAllPropValues("quantity");
+//   getAllPropValues("price");
+//   getAllPropValues("category");
+
+// const products = [
+//     { name: "Radar", price: 1300, quantity: 4 },
+//     { name: "Scanner", price: 2700, quantity: 3 },
+//     { name: "Droid", price: 400, quantity: 7 },
+//     { name: "Grip", price: 1200, quantity: 9 },
+//   ];
+  
+//   let totalPrice = 0;
+  
+//   function calculateTotalPrice(productName) {
+//     // Change code below this line
+//   for (const product of products) {
+//     if (product.name === productName) {
+//       totalPrice =+ product.price * product.quantity;
+//       console.log(totalPrice);
+//     }
+    
+//   }
+  
+  
+//     // Change code above this line
+//   }
+//   calculateTotalPrice("Blaster");
+//   calculateTotalPrice("Radar");
+//   calculateTotalPrice("Droid");
+//   calculateTotalPrice("Grip");
+//   calculateTotalPrice("Scanner");
+
+function addOverNum(...args) {
+    let total = 0;
+  const number = args[0];
+  console.log(number);
+    for (const arg of args) {
+      if (number < arg) {
+        total += arg;
+      }
+      
+    }
+  
+    return total;
+    // Change code above this line
+  }
+  addOverNum(50, 15, 27);
+  addOverNum(10, 12, 4, 11, 48, 10, 8);
+  addOverNum(15, 32, 6, 13, 19, 8);
+  addOverNum(20, 74, 11, 62, 46, 12, 36) ;
--- lesson5.js
+++ lesson5.js
@@ -39,20 +39,88 @@
 //   console.log(values);
 // };
 
-function countProps(object) {
-    let propCount = 0;
-    // Change code below this line
-  for (const key in object) {
-    if (object.hasOwnProperty(key)) {
-        propCount += 1;
-      }
-  }
-    // Change code above this line
-    return propCount;
-  }
-  
-  countProps({});
-  countProps({ name: "Mango", age: 2 });
-  countProps({ mail: "poly@mail.com", isOnline: true, score: 500 });
+// const apartment = {
+//   descr: "Spacious apartment in the city center",
+//   rating: 4,
+//   price: 2153,
+// };
+// const values = [];
+// // Change code below this line
+// const keys = Object.keys(apartment);
 
+// for (const key of keys) {
+//   console.log(apartment[key]);
+//   values.push(apartment[key])
+//   console.log(values);
+// }
+
+// function countTotalSalary(salaries) {
+//   let totalSalary = 0;
+//   // Change code below this line
+// const name = Object.keys(salaries);
+//   const salary = Object.values(salaries);
+//   console.log(salary);
+//   for (const sum of salary) {
+//     console.log(sum);
+//     totalSalary += sum;
+//     console.log(totalSalary);
+//   }
+  
+//   // Change code above this line
+//   return totalSalary;
+// }
+
+// countTotalSalary({});
+// countTotalSalary({ mango: 100, poly: 150, alfred: 80 });
+// countTotalSalary({ kiwi: 200, poly: 50, ajax: 150 });
+
+// const colors = [
+//   { hex: "#f44336", rgb: "244,67,54" },
+//   { hex: "#2196f3", rgb: "33,150,243" },
+//   { hex: "#4caf50", rgb: "76,175,80" },
+//   { hex: "#ffeb3b", rgb: "255,235,59" },
+// ];
+
+// const hexColors = [];
+// const rgbColors = [];
+// // Change code below this line
+// for (let color of colors) {
+//   console.log(color.hex);
+//   hexColors.push(color.hex);
+//   console.log(hexColors);
+// }
+
+// const products = [
+//   { name: "Radar", price: 1300, quantity: 4 },
+//   { name: "Scanner", price: 2700, quantity: 3 },
+//   { name: "Droid", price: 400, quantity: 7 },
+//   { name: "Grip", price: 1200, quantity: 9 },
+// ];
+
+
+
+// function getProductPrice(productName) {
+//   // Change code below this line
+// for (const product of products) {
+//   // console.log(product);
+//   const prodName = product.name;
+//   console.log(prodName);
+//   if (productName === prodName) {
+    
+    
+//     return product.price;
+//   }
+  
+//   return null;
+// }
+
+
+//   // Change code above this line
+// }
+
+// getProductPrice("Radar");
+// getProductPrice("Grip");
+// getProductPrice("Scanner");
+// getProductPrice("Droid");
+// getProductPrice("Engine");
   
